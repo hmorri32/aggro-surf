5626a48c4d1a58a740dc76fda64fe2f0b55501aa
Hugh Morrison <hmorri32@gmail.com>
2017-04-12 19:11:00 -0600

    horrendous line chart. dale bro.

--- src/components/dataViz/DataViz.js
+++ src/components/dataViz/DataViz.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import { VictoryLine } from 'victory';
+import RC2 from 'react-chartjs2';
+import './DataViz.css' 
+
 
 class DataViz extends Component {
+
   render() {
+
+    const mapped = this.props.tides.map((stuff) => {
+      return stuff.tide
+    })
+
+    const hourly = this.props.tides.map((tide) => {
+      return tide.hour
+    })
+
+    const data = {
+      labels: hourly,
+      datasets: [
+        {
+          label: 'SD County Tides',
+          fill: false,
+          lineTension: 0.1,
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: mapped,
+        }
+      ]
+    };
+
+
+    let reduced = mapped.reduce((obj, stuff) => {
+      obj[stuff]
+      return obj
+    }, {})
+
     return (
-      <div>Suh dude</div>
+      <div className='tides'>
+        <RC2 data={data} type='line' />
+      </div>
     )
   }
 }
