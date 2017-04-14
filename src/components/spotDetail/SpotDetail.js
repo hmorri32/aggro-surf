5f0e38947621ed8701f6a714be439e13a760fff3
Hugh Morrison <hmorri32@gmail.com>
2017-04-14 17:01:30 -0600

    windyty overlay for that yung spotdetail.

--- src/components/spotDetail/SpotDetail.js
+++ src/components/spotDetail/SpotDetail.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {WelcomeScreen} from '../welcomeScreen/WelcomeScreen';
-import {yungKeys} from '../../helpers/keys.js'
+import {yungKeys} from '../../helpers/keys.js';
+import './SpotDetail.css'
 
 class SpotDetail extends Component {
   constructor() {
@@ -21,12 +22,14 @@ class SpotDetail extends Component {
     })
   }
 
-  renderForecast(){
-    {!this.state.forecast ? null : this.myComponent()}
+  createMarkup() {
+    if(this.state.forecast) {
+      return {__html: `${this.state.forecast.Analysis.short_term_forecast}`};
+    }
   }
 
-  myComponent() {
-    return <div dangerouslySetInnerHTML={this.state.forecast.Analysis.short_term_forecast} />;
+  MyComponent() {
+    return <div dangerouslySetInnerHTML={this.createMarkup()} />;
   }
 
   render() {
@@ -34,15 +37,23 @@ class SpotDetail extends Component {
       <div className='tides'>
         <WelcomeScreen />
         <div id='render'>
-          {this.renderForecast()}
+          {this.MyComponent()}
         </div>
-        <iframe
-          width="100%"
-          height="450"
-          frameBorder="0"
-          src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}
+        <div className='frames'>
+          <iframe
+            width="100%"
+            height="650"
+            frameBorder="0"
+            type="satellite"
+            src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}
             &q=${this.state.forecast.lat}, ${this.state.forecast.lon}`}>
-        </iframe>
+          </iframe>
+          <iframe
+            width="100%"
+            height="650"  src={`https://embed.windytv.com/embed2.html?lat=${this.state.forecast.lat}&lon=${this.state.forecast.lon}&zoom=13&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C`}
+            frameBorder="0">
+          </iframe>
+        </div>
       </div>
     )
   }
