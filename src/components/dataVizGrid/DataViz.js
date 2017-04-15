68821c3edf6bfe86da60c2d5db40cf287f9dc3c0
Hugh Morrison <hmorri32@gmail.com>
2017-04-15 16:34:33 -0600

    yung fixed nav bar

--- src/components/dataVizGrid/DataViz.js
+++ src/components/dataVizGrid/DataViz.js
@@ -5,11 +5,15 @@ import './DataViz.css';
 
 class dataVizGrid extends Component {
 
+  componentDidMount() {
+    window.scrollTo(0,0);
+  }
+
   render() {
     return (
       <div>
         <div className='tides'>
-          <h2 className='SD-tides'>Saint Diego Tides (dealwithit)</h2>
+          <h2 className='SD-tides'>Saint Diego County (dealwithit)</h2>
           {/* <TideVizContainer /> */}
         </div>
         <div className='tides forecast'>
--- src/components/spotDetail/SpotDetail.js
+++ src/components/spotDetail/SpotDetail.js
@@ -15,6 +15,7 @@ class SpotDetail extends Component {
   }
 
   componentDidMount() {
+    window.scrollTo(0,0);
     fetch(`http://api.surfline.com/v1/forecasts/${this.props.match.params.id}`)
     .then(response => response.json())
     .then((json) => {
@@ -36,28 +37,31 @@ class SpotDetail extends Component {
 
   render() {
     return (
-      <div className='tides'>
+      <div>
+
         <WelcomeScreen />
-        <div id='render'>
-          <h2>Report</h2>
-          {!this.state.forecast ? null : <p>{this.state.forecast.Analysis.reportdate }</p>}
-          {this.MyComponent()}
-          <TideVizContainer />
-        </div>
-        <div className='frames'>
-          <iframe
-            className='frames'
-            width="100%"
-            height="650"
-            frameBorder="0"
-            src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}&q=${this.state.forecast.lat}, ${this.state.forecast.lon}&maptype=satellite`}>
-          </iframe>
-          <iframe
-            className='frames'
-            width="100%"
-            height="650"  src={`https://embed.windytv.com/embed2.html?lat=${this.state.forecast.lat}&lon=${this.state.forecast.lon}&zoom=13&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C`}
-            frameBorder="0">
-          </iframe>
+        <div className='tides padding'>
+          <div id='render'>
+            <h2>Report</h2>
+            {!this.state.forecast ? null : <p>{this.state.forecast.Analysis.reportdate }</p>}
+            {this.MyComponent()}
+            <TideVizContainer />
+          </div>
+          <div className='frames'>
+            <iframe
+              className='frames'
+              width="100%"
+              height="650"
+              frameBorder="0"
+              src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}&q=${this.state.forecast.lat}, ${this.state.forecast.lon}&maptype=satellite`}>
+            </iframe>
+            <iframe
+              className='frames'
+              width="100%"
+              height="650"    src={`https://embed.windytv.com/embed2.html?lat=${this.state.forecast.lat}&lon=${this.state.forecast.lon}&zoom=13&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C`}
+              frameBorder="0">
+            </iframe>
+          </div>
         </div>
       </div>
     )
