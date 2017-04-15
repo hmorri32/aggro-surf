719c46063cb483fae8a709958f7d76ece9535b38
Hugh Morrison <hmorri32@gmail.com>
2017-04-15 13:10:28 -0600

    nathanielify montage

--- src/components/montageLife/MontageLife.js
+++ src/components/montageLife/MontageLife.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Montage.css';
-import hangfive from './assets/aggro-pix/hangfive.jpg'
+import React             from 'react';
+import hangfive          from './assets/aggro-pix/hangfive.jpg'
 import abstractblueblack from './assets/aggro-pix/abstractblueblack.jpg'
-import abstractoil from './assets/aggro-pix/abstractoil.jpg'
-import boardslidebrah from './assets/aggro-pix/boardslidebrah.jpg'
-import breakwall from './assets/aggro-pix/breakwall.jpg'
-import chic from './assets/aggro-pix/chic.jpg'
-import dusk from './assets/aggro-pix/dusk.jpg'
-import hipsters from './assets/aggro-pix/hipsters.jpg'
-import jesussurfs from './assets/aggro-pix/jesussurfs.jpg'
-import palms from './assets/aggro-pix/palms.jpg'
-import snapper from './assets/aggro-pix/snapper.jpg'
-import pipe from './assets/aggro-pix/pipe.jpg'
-import ship from './assets/aggro-pix/ship.jpg'
-import tubez from './assets/aggro-pix/tubez.jpg'
+import abstractoil       from './assets/aggro-pix/abstractoil.jpg'
+import boardslidebrah    from './assets/aggro-pix/boardslidebrah.jpg'
+import breakwall         from './assets/aggro-pix/breakwall.jpg'
+import chic              from './assets/aggro-pix/chic.jpg'
+import dusk              from './assets/aggro-pix/dusk.jpg'
+import hipsters          from './assets/aggro-pix/hipsters.jpg'
+import jesussurfs        from './assets/aggro-pix/jesussurfs.jpg'
+import palms             from './assets/aggro-pix/palms.jpg'
+import snapper           from './assets/aggro-pix/snapper.jpg'
+import pipe              from './assets/aggro-pix/pipe.jpg'
+import ship              from './assets/aggro-pix/ship.jpg'
+import tubez             from './assets/aggro-pix/tubez.jpg'
+import './Montage.css';
 
 const MontageLife = () => {
   return (
@@ -26,7 +26,6 @@ const MontageLife = () => {
         frameBorder="0"
         allowFullScreen>
       </iframe>
-      <img src={boardslidebrah} className='vidz'/>
       <iframe
         className='vidz'
         src="https://player.vimeo.com/video/72287506?color=ffffff&byline=0&portrait=0"
@@ -35,7 +34,15 @@ const MontageLife = () => {
         frameBorder="0"
         allowFullScreen>
       </iframe>
-      <img src={breakwall} className='vidz'/>
+      <iframe
+        className='vidz'
+        src="https://player.vimeo.com/video/90429499?color=ffffff&byline=0&portrait=0"
+        width="640"
+        height="360"
+        frameBorder="0"
+        allowFullScreen>
+      </iframe>
+      <img src={boardslidebrah} className='vidz'/>
       <img src={chic} className='vidz'/>
       <img src={abstractoil} className='vidz' />
       <img src={dusk} className='vidz' />
