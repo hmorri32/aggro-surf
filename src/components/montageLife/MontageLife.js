e3ec158af1f09bf40e70025f39aa427296fccadf
Hugh Morrison <hmorri32@gmail.com>
2017-04-15 12:16:52 -0600

    style that yung montage

--- src/components/montageLife/MontageLife.js
+++ src/components/montageLife/MontageLife.js
@@ -5,6 +5,7 @@ const MontageLife = () => {
   return (
     <div className='montage'>
       <iframe
+        className='vidz'
         src="https://player.vimeo.com/video/200922358?color=ffd9db&byline=0&portrait=0"
         width="640"
         height="360"
@@ -14,7 +15,20 @@ const MontageLife = () => {
         allowfullscreen>
     </iframe>
     <iframe
+              className='vidz'
+
       src="https://player.vimeo.com/video/151367434?color=ffffff&byline=0&portrait=0"
+      width="640"
+      height="360"
+      frameborder="0"
+      webkitallowfullscreen
+      mozallowfullscreen
+      allowfullscreen>
+    </iframe>
+    <iframe
+              className='vidz'
+
+      src="https://player.vimeo.com/video/151743858?color=ffffff&byline=0&portrait=0"
       width="640"
       height="360"
       frameborder="0"
