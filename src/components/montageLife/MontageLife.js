104851517f8e88bb1ca8f64d50a6875ec3f4a6d1
Hugh Morrison <hmorri32@gmail.com>
2017-04-15 12:59:58 -0600

    montagein

--- src/components/montageLife/MontageLife.js
+++ src/components/montageLife/MontageLife.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Montage.css';
-import abstract from './assets/aggro-pix/abstract.jpg'
+import hangfive from './assets/aggro-pix/hangfive.jpg'
+import abstractblueblack from './assets/aggro-pix/abstractblueblack.jpg'
+import abstractoil from './assets/aggro-pix/abstractoil.jpg'
+import boardslidebrah from './assets/aggro-pix/boardslidebrah.jpg'
+import breakwall from './assets/aggro-pix/breakwall.jpg'
+import chic from './assets/aggro-pix/chic.jpg'
+import dusk from './assets/aggro-pix/dusk.jpg'
+import hipsters from './assets/aggro-pix/hipsters.jpg'
+import jesussurfs from './assets/aggro-pix/jesussurfs.jpg'
+import palms from './assets/aggro-pix/palms.jpg'
+import snapper from './assets/aggro-pix/snapper.jpg'
 
 const MontageLife = () => {
   return (
@@ -12,25 +22,43 @@ const MontageLife = () => {
         height="360"
         frameBorder="0"
         allowFullScreen>
-    </iframe>
-    <iframe
-      className='vidz'
-      src="https://player.vimeo.com/video/151367434?color=ffffff&byline=0&portrait=0"
-      width="640"
-      height="360"
-      frameBorder="0"
-      allowFullScreen>
-    </iframe>
-    <img src={abstract} />
-    <iframe
-      className='vidz'
-      src="https://player.vimeo.com/video/151743858?color=ffffff&byline=0&portrait=0"
-      width="640"
-      height="360"
-      frameBorder="0"
-      allowFullScreen>
-    </iframe>
-  </div>
+      </iframe>
+      <img src={boardslidebrah} className='vidz'/>
+      <iframe
+        className='vidz'
+        src="https://player.vimeo.com/video/72287506?color=ffffff&byline=0&portrait=0"
+        width="640"
+        height="360"
+        frameBorder="0"
+        allowFullScreen>
+      </iframe>
+      <img src={breakwall} className='vidz'/>
+      <img src={chic} className='vidz'/>
+      <img src={abstractoil} className='vidz' />
+      <img src={dusk} className='vidz' />
+      <iframe
+        className='vidz'
+        src="https://player.vimeo.com/video/151367434?color=ffffff&byline=0&portrait=0"
+        width="640"
+        height="360"
+        frameBorder="0"
+        allowFullScreen>
+      </iframe>
+      <img src={hangfive} className='vidz'/>
+      <img src={abstractblueblack} className='vidz' />
+      <img src={hipsters} className='vidz'/>
+      <iframe
+        className='vidz'
+        src="https://player.vimeo.com/video/151743858?color=ffffff&byline=0&portrait=0"
+        width="640"
+        height="360"
+        frameBorder="0"
+        allowFullScreen>
+      </iframe>
+      <img src={palms} className='vidz'/>
+      <img src={jesussurfs} className='vidz'/>
+      <img src={snapper} className='vidz'/>
+    </div>
   )
 }
 
