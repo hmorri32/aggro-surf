79676e30a1d21e6d759adbc7756bdbd9aecfb128
Hugh Morrison <hmorri32@gmail.com>
2017-04-14 18:12:43 -0600

    torch google maps

--- src/components/spotDetail/SpotDetail.js
+++ src/components/spotDetail/SpotDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {WelcomeScreen} from '../welcomeScreen/WelcomeScreen';
-import {yungKeys} from '../../helpers/keys.js';
+import { Link }             from 'react-router-dom';
+import { WelcomeScreen }    from '../welcomeScreen/WelcomeScreen';
+import { yungKeys }         from '../../helpers/keys.js';
 import './SpotDetail.css'
 
 class SpotDetail extends Component {
