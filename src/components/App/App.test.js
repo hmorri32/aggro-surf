80b9045c20f58204fb5842657a9e001d02be4cd7
Hugh Morrison <hmorri32@gmail.com>
2017-04-18 17:25:31 -0600

    install all test dependencies

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React              from 'react'
+import { shallow }        from 'enzyme'
+import fetchMock          from 'fetch-mock'
+import { browserHistory } from 'react-router'
+import App from './App.js'
 
 it('renders without crashing', () => {
   let wrapper = shallow(<App />)
-
+  console.log(wrapper.debug());
 });
